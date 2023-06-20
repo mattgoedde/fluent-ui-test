b62839523b52452df206cd145cd037102d1e6a86
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-06-20 07:48:16 -0400

    add icon generator code

--- Test.FluentUi/Program.cs
+++ Test.FluentUi/Program.cs
@@ -6,6 +6,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddFluentUIComponents(options =>
+{
+    options.HostingModel = {see remark below};
+    options.IconConfiguration = ConfigurationGenerator.GetIconConfiguration();
+    options.EmojiConfiguration = ConfigurationGenerator.GetEmojiConfiguration();
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
